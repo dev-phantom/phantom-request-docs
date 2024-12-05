29558fe2d2f1f1fc57310a4d35439de98bb189d7
dev-phantom <godsfavour1975@gmail.com>
2024-12-05 15:56:48 +0100

    fix: fixed dark theme for installation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -127,7 +127,7 @@ function Features() {
 
 function Installation() {
   return (
-    <section className="py-16 bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
+    <section className="py-16 bg-gradient-to-br from-gray-100 via-gray-200 to-gray-300 text-gray-900 dark:from-gray-900 dark:via-gray-800 dark:to-black dark:text-white">
       <div className="container mx-auto px-6 sm:px-10">
         <motion.h2
           className="text-4xl font-extrabold text-center sm:text-5xl"
@@ -139,34 +139,37 @@ function Installation() {
           Installation
         </motion.h2>
         <motion.p
-          className="mt-6 text-lg text-center text-gray-300 sm:text-xl"
+          className="mt-6 text-lg text-center text-gray-700 dark:text-gray-300 sm:text-xl"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           viewport={{ once: true }}
           transition={{ duration: 1, delay: 0.2 }}
         >
           Get started by installing{" "}
-          <span className="font-bold text-green-400">phantom-request</span> via
-          npm or yarn:
+          <span className="font-bold text-green-600 dark:text-green-400">
+            phantom-request
+          </span>{" "}
+          via npm or yarn:
         </motion.p>
         <motion.div
-          className="mt-10 bg-gray-800 rounded-xl p-6 sm:p-8 shadow-lg"
+          className="mt-10 bg-gray-100 dark:bg-gray-800 rounded-xl p-6 sm:p-8 shadow-lg"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 1, delay: 0.4 }}
         >
-          <div className="font-mono">NPM:</div>
-          <pre className="text-lg font-mono bg-gray-900  text-gray-100 p-4 rounded-lg">
+          <div className="font-mono text-gray-800 dark:text-gray-100">NPM:</div>
+          <pre className="text-lg font-mono bg-gray-200 dark:bg-gray-900 text-gray-800 dark:text-gray-100 p-4 rounded-lg">
             <code className="block">
-              <span className="text-green-400">$</span> npm install
-              phantom-request
+              <span className="text-green-600 dark:text-green-400">$</span> npm
+              install phantom-request
             </code>
           </pre>
-          <div className="font-mono">Yarn:</div>
-          <pre className="text-lg font-mono bg-gray-900  text-gray-100 p-4 rounded-lg">
+          <div className="font-mono text-gray-800 dark:text-gray-100">Yarn:</div>
+          <pre className="text-lg font-mono bg-gray-200 dark:bg-gray-900 text-gray-800 dark:text-gray-100 p-4 rounded-lg">
             <code className="block">
-              <span className="text-green-400">$</span> yarn add phantom-request
+              <span className="text-green-600 dark:text-green-400">$</span> yarn
+              add phantom-request
             </code>
           </pre>
         </motion.div>
@@ -179,7 +182,7 @@ function Installation() {
         >
           <a
             href="/docs/intro"
-            className="inline-block px-6 py-3 hover:no-underline text-lg font-medium text-black bg-green-400 rounded-lg shadow-md hover:bg-green-500"
+            className="inline-block px-6 py-3 text-lg font-medium text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700 dark:text-black dark:bg-green-400 dark:hover:bg-green-500"
           >
             View Documentation 📚
           </a>

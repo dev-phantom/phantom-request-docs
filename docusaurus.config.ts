fbd58f12283070c765ee6c67f264cbc1628ef77a
dev-phantom <godsfavour1975@gmail.com>
2024-12-05 18:15:38 +0100

    fix: added x account

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,17 +104,14 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
               label: 'X',
-              href: 'https://x.com/docusaurus',
+              href: 'https://x.com/phantom_favour',
             },
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/phantom-request',
+            },
+           
           ],
         },
         {

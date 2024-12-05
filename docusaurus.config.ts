d775af634dd66cbd3a8bb329b7ead709c0aa1d11
dev-phantom <godsfavour1975@gmail.com>
2024-12-05 18:17:55 +0100

    fix: fixed some stuff

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/dev-phantom/phantom-request-docs/edit/master/docs/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
